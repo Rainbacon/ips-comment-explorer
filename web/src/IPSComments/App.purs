da91a1e06d2fd99d70bb102418b66156378cdbc1
Aaron Reynolds <reynolds.aaron1992@gmail.com>
2019-09-26 21:28:52 -0400

    Switched frontend to react

--- web/src/IPSComments/App.purs
+++
@@ -1,5 +0,0 @@
-module IPSComments.App where
-
-import React.DOM as D
-import React
-
---
+++ web/src/reducers.js
@@ -0,0 +1,10 @@
+import { createStore } from 'redux';
+
+const initialState = {};
+
+function appReducer(state=initialState, action) {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
